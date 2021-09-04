b08420005223bf48453843be7be12bee6af34997
huangdijia <huangdijia@gmail.com>
2021-09-05 00:02:27 +0800

    Fixed in PHP8.0

--- bootstrap.php
+++ bootstrap.php
@@ -15,7 +15,7 @@ use Hyperf\Di\Aop\ProxyCallVisitor;
 use Hyperf\Di\Aop\RegisterInjectPropertyHandler;
 
 ! defined('BASE_PATH') && define('BASE_PATH', __DIR__);
-! defined('SWOOLE_HOOK_FLAGS') && define('SWOOLE_HOOK_FLAGS', SWOOLE_HOOK_ALL);
+// ! defined('SWOOLE_HOOK_FLAGS') && define('SWOOLE_HOOK_FLAGS', SWOOLE_HOOK_ALL);
 
 require_once BASE_PATH . '/vendor/autoload.php';
 
